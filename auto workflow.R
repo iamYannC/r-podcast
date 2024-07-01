d6ed703fcbb8b7ad540c381abf724ed967ca403d
Yann <yannco5@gmail.com>
2024-07-01 18:25:38 +0300

    setting relative path

--- auto workflow.R
+++ auto workflow.R
@@ -1,8 +1,8 @@
 library(rvest)
 r_weekly <- rvest::read_html("https://serve.podhome.fm/r-weekly-highlights")
-n_episdoes <- nrow(readxl::read_xlsx('C:/Users/97253/Documents/R/r-podcast/data/all_data.xlsx'))
+n_episdoes <- nrow(readxl::read_xlsx('data/all_data.xlsx'))
 
 if(length(r_weekly |> html_elements(".episodeLink")) > n_episdoes){
 
 # re-run the whole scrape
-source('C:/Users/97253/Documents/R/r-podcast/r weekly podcast scrape - followup.R')
+source('r weekly podcast scrape - followup.R')
