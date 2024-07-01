75e9d4de8f627209f694b1ae8bafdae8d6f81402
Yann <yannco5@gmail.com>
2024-07-01 19:15:19 +0300

    removing lib rvest

--- auto workflow.R
+++ auto workflow.R
@@ -1,8 +1,7 @@
-library(rvest)
 r_weekly <- rvest::read_html("https://serve.podhome.fm/r-weekly-highlights")
 n_episdoes <- nrow(readxl::read_xlsx('data/all_data.xlsx'))
 
-if(length(r_weekly |> html_elements(".episodeLink")) > n_episdoes){
+if(length(r_weekly |> rvest::html_elements(".episodeLink")) > n_episdoes){
 
 # re-run the whole scrape
 source('r weekly podcast scrape - followup.R')
