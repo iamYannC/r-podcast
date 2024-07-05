10419adab623208369d71d88080e5bc9a43a2d74
Yann <yannco5@gmail.com>
2024-07-06 01:15:40 +0300

    adding r auto workflow file

--- auto workflow.R
+++ auto workflow.R
@@ -1,10 +1,14 @@
+library(rvest)
 r_weekly <- rvest::read_html("https://serve.podhome.fm/r-weekly-highlights")
 n_episdoes <- nrow(readxl::read_xlsx('data/all_data.xlsx'))
 
-if(length(r_weekly |> rvest::html_elements(".episodeLink")) > n_episdoes){
+if(length(r_weekly |> html_elements(".episodeLink")) > n_episdoes){
 
 # re-run the whole scrape
 source('r weekly podcast scrape - followup.R')
+
 } else{
-  print("No new episodes found")
+x <- Sys.time()
+writeLines(paste('generated @',x),'data/last run.txt')
+
 }
