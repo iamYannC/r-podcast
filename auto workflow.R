361320386713f89470bc7cab659275879cb0f223
Yann <yannco5@gmail.com>
2024-07-08 22:35:06 +0300

    styling file

--- auto workflow.R
+++ auto workflow.R
@@ -1,15 +1,15 @@
 require(rvest)
 r_weekly <- rvest::read_html("https://serve.podhome.fm/r-weekly-highlights")
-n_episdoes <- nrow(readxl::read_xlsx('data/all_data.xlsx'))
+n_episdoes <- nrow(readxl::read_xlsx("data/all_data.xlsx"))
 
-if(length(r_weekly |> html_elements(".episodeLink")) > n_episdoes){
-
-# re-run the whole scrape
-source('r weekly podcast scrape - followup.R')
-
-} else{
-x <- Sys.time()
-writeLines(paste('Number of episodes:', n_episdoes-1,'@',x) |> 
-          sub("\\..*","",x = _)
-          ,'data/last run.txt')
+if (length(r_weekly |> html_elements(".episodeLink")) > n_episdoes) {
+  # re-run the whole scrape
+  source("r weekly podcast scrape - followup.R")
+} else {
+  x <- Sys.time()
+  writeLines(
+    paste("Number of episodes:", n_episdoes - 1, "@", x) |>
+      sub("\\..*", "", x = _),
+          "data/last run.txt"
+  )
 }
