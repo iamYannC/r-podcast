addb90e450c3ca8c2f6d6acf82e7d82cd3cf1947
Yann <yannco5@gmail.com>
2024-07-14 04:00:08 +0300

    cut video becomes split video to chuncks

--- Py/cut audio.py
+++ Py/cut audio.py
@@ -2,17 +2,7 @@ from imports import *
 from functions import *
 
 input_file = r"fulll eps\24-w26.mp3"
-output_file = r"cut eps\conv_cut.mp3"
-output_folder = r"cut eps"
-
-start_time = 0  
-end_time = to_milliseconds(10)   
-
-# cut_mp3(input_file, output_file, start_time, end_time)
-
-
-
-# Example usage
+output_folder = "cut eps"
 
 # Split into 10-minute chunks (600000 milliseconds)
 split_mp3(input_file, output_folder)
